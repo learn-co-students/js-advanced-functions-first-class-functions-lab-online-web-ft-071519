e19f6a3e791503cd805d7bd8fad50362c8ff7b3d
BaronLip <baronlip@gmail.com>
2019-10-29 22:51:36 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(driversArray) {
+    return driversArray.slice(0, 2)
+}
+
+const returnLastTwoDrivers = function(driversArray) {
+    return driversArray.slice(Math.max(driversArray.length - 2), driversArray.length)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function(multiplier) {
+    return function(fare) {
+        return fare * multiplier;
+    }
+}
+
+fareDoubler = function(fare) {
+    return fare * 2;
+}
+
+function fareTripler(fare) {
+    return fare * 3;
+}
+
+// I believe something is wrong with the test.
+// const selectDifferentDrivers = function(driversArray, driverFunction) {
+//     return driverFunction(driversArray);
+// }
+// ***Nevermind it needed to be a function and not a variable of a function...sigh.
+function selectDifferentDrivers(driversArray, driverFunction) {
+    return driverFunction(driversArray);
+}
+
